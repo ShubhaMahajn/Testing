05d1b664df1eaf0e2e5ce479b396379fef3bfb0f
ShubhaMahajn <142983152+ShubhaMahajn@users.noreply.github.com>
2023-08-23 15:29:02 +0530

    Update GitDemo.java

--- GitProject/src/Demo/GitDemo.java
+++ GitProject/src/Demo/GitDemo.java
@@ -1,6 +1,12 @@
 package Demo;
 
-public class GitDemo {
+public class GitDemo 
+{
+	public void m1()
+	{
+		System.out.println("github");
+
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
